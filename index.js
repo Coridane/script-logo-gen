ee1b5e4af651baccf8f7df633e4ec19881791fef
dmwilliams <danielmarkwilliams@gmail.com>
2023-07-29 01:32:05 -0500

    Update files with comments and fixed typo

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-// These are the packages that we need. Inquirer for the questions,
-// generateMarkdown for the HTML, and fs to write the file.
+// These are the packages, files, or destination path that we need. Inquirer is a package needed for the questions.
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const questions = require("./lib/questions.js");
 const createShape = require("./lib/createShape.js");
 const fileName = "./dist/logo.svg";
 
+// This will write to the file path and name above, using the response we get from createShape.js.
+
 function createLogo(response) {
     const svg = createShape(response);
     fs.writeFile(fileName, svg, () => console.log("Generated dist/logo.svg"));    
 }
 
+// init() will send us here wheere Inquirer will ask questions (questions.js).
+
 function init() {
     inquirer.prompt(questions)
+
+// Then it will execute the createLogo function above.
+
     .then((response) => {
         createLogo(response);        
     })
@@ -20,5 +27,7 @@ function init() {
         console.log(err)
     });
 }
+
+// This is where we start our journey.
 
 init();
